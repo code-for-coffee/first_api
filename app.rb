b7684197964520ef97f5f41f16a30cc1b9b895f6
James Traver <jtraverjr@gmail.com>
2015-07-23 12:23:15 -0500

    my first server

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'Bundler'
+Bundler.require()
+
+# http://localhost/
+get '/' do
+  {:name => 'Darth Vader', :message => 'The force is strong with this one'}.to_json
+end
+
+# http://localhost/api/awesome/
+get '/api/awesome' do
+  # do something else
+end
+
+# https://me.github.io/project1/
+# get '/project1/' do
+#   stuff
+# end
